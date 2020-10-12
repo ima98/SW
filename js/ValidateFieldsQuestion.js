317700b2306fa93f6183296803bc37fd3b367c20
Imanol <Imanol.guriezo@gmail.com>
2020-10-12 12:14:46 +0200

    sw

---
+++ js/ValidateFieldsQuestion.js
@@ -0,0 +1,62 @@
+/*$(document).ready(function(){
+  $("#submit").click(function(){
+
+    return ( validarCampoVacio() && validarPregunta($('#pregunta').val()) && validarCorreo($('#correo').val()) ) ;
+  });
+});
+*/
+
+function mainM(){
+
+     if(!validarCampoVacio()){
+       alert("Hay algun campo vacio");
+       return false;
+
+     }else if(!validarCorreo($('#correo').val())){
+        alert("El correo electronico introducido no es valido.");
+      return false;
+
+     }else if(! validarPregunta($('#pregunta').val())){
+        alert("La pregunta tiene que contener al menos 10 caracteres");
+       return false;
+     }else{
+       return true;
+     }
+     return true;
+   
+   });
+ });
+ }
+
+
+function validarCampoVacio(){
+  $("form :input").each(function(){
+        var emp = $(this).val();
+        if(emp.length==0){
+            alert("El campo "+ "\' "+ emp.attr("name")+"\'"+" esta vacio.");
+            return false;
+        }
+        return true;
+    });
+}
+
+ function validarPregunta(preg){
+  if(preg.length <10){
+     alert("La pregunta tiene que contener al menos 10 caracteres");
+    return false;
+  }
+return true;
+ }
+
+
+ function validarCorreo(email){
+    var exprAlu = /^[a-zA-Z]+(([0-9]{3})+@ikasle\.ehu\.(eus|es))$/;
+    var exprPro = /^[a-zA-Z]+(\.[a-zA-Z]+@ehu\.(eus|es)|@ehu\.(eus|es))$/;
+
+    if(exprAlu.test(email)||exprPro.test(email)){
+        return true;
+    }
+
+    alert("El correo electronico introducido no es valido.");
+    return false;
+}
---
+++ php/Credits.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include '../html/Head.html'?>
+</head>
+<body>
+  <?php include '../php/Menus.php' ?>
+  <section class="main" id="s1">
+    <div>
+
+      <h2>DATOS DEL AUTOR/AUTORES</h2>
+	  <p>Imanol Lavin Lertxundi               Aitor Ortega Rodriguez</p> 
+	  <p style="text-align:right"></p>
+	  <h4>Especialidad de grado Software</h4>
+	  <img src="https://pbs.twimg.com/media/EhQu7R_WkAMfFZ5?format=jpg&name=large" width="300" height="400"><br>
+	  <a href="mailto:ilavin005@ikasle.ehu.eus">ilavin005@ikasle.ehu.eus</a>
+	  aortega073@ikasle.ehu.eus
+	  <h6>
+	  
+	  
+	  
+
+    </div>
+  </section>
+  <?php include '../html/Footer.html' ?>
+</body>
+</html>
