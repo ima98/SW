01c867cddc445d29eb356583247c71b4ac5590be
Imanol <Imanol.guriezo@gmail.com>
2020-10-20 14:57:33 +0200

    ver las preguntas

--- php/ShowQuestions.php
+++ php/ShowQuestions.php
@@ -1,16 +1,65 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <?php include '../html/Head.html'?>
+	<?php include '../html/Head.html'?>
 </head>
 <body>
-  <?php include '../php/Menus.php' ?>
-  <section class="main" id="s1">
-    <div>
-      Código PHP para mostrar una tabla con las preguntas de la BD.<br>
-      La tabla no incluye las imágenes
-    </div>
-  </section>
-  <?php include '../html/Footer.html' ?>
+	<?php include '../php/Menus.php' ?>
+	<section class="main" id="s1">
+		<div>
+			<?php
+			$link = mysqli_connect("localhost", "root", "", "quiz");
+
+			$query = mysqli_query($link, "SELECT * FROM preguntas" );
+			//or die(mysqli_error($link));
+			
+
+			//if($query) {
+				echo"
+				<table><tr>
+				<th>Correo</th> 
+				<th>Tema</th>
+				<th>Pregunta</th>
+				<th>Opcion 1</th>
+				<th>Opcion 2</th>
+				<th>Opcion 3</th>
+				<th>Opcion 4</th>
+				<th>Dificultad</th>
+				</tr>
+				";
+
+				while ($row = mysqli_fetch_array($query)) {
+					/*echo "<tr>
+					<td>" . $row["correo"]. "</td>
+					<td>" . $row["tema"]. "</td>
+					<td> " . $row["pregunta"]. "</td>
+					<td>" . $row["correcta"]. "</td>
+					<td>" . $row["incorrecta1"]. "</td>
+					<td>" . $row["incorrecta2"]. "</td>
+					<td>" . $row["incorrecta3"]. "</td>
+					<td>" . $row["dificultad"]. "</td>
+					</tr>";
+					*/
+
+					echo "<tr>
+					<td>" . $row[0]. "</td>
+					<td>" . $row[1]. "</td>
+					<td> " . $row[2]. "</td>
+					<td>" . $row[3]. "</td>
+					<td>" . $row[4]. "</td>
+					<td>" . $row[5]. "</td>
+					<td>" . $row[6]. "</td>
+					<td>" . $row[7]. "</td>
+					</tr>";
+					
+				}
+
+				echo '</table>';
+				$query->close();
+				mysqli_close($link);
+			?>
+		</div>
+	</section>
+	<?php include '../html/Footer.html' ?>
 </body>
 </html>
